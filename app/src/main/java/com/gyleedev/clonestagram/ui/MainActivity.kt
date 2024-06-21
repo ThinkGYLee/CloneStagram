8b81538aac16fe6e8d9dae6a7c5bb02939ec68fa
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-22 01:07:47 +0900

    - MainActivity에 CloneStagramScreen 연결

--- app/src/main/java/com/gyleedev/clonestagram/ui/MainActivity.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/MainActivity.kt
@@ -4,10 +4,6 @@ import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.ui.Modifier
 import com.gyleedev.clonestagram.ui.theme.CloneStagramTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -18,11 +14,7 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContent {
             CloneStagramTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    CloneStagramScreen(
-                        Modifier.padding(innerPadding)
-                    )
-                }
+                CloneStagramScreen()
             }
         }
     }
