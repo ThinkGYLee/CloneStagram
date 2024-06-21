5e897a5ab7c955ba5466a62fdc9d342131488ffb
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-21 23:50:33 +0900

    - CloneStagramScreen에 modifier 추가

--- app/src/main/java/com/gyleedev/clonestagram/ui/CloneStagramScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/CloneStagramScreen.kt
@@ -1,8 +1,11 @@
 package com.gyleedev.clonestagram.ui
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 @Composable
-fun CloneStagramScreen() {
+fun CloneStagramScreen(
+    modifier: Modifier
+) {
 
 }
