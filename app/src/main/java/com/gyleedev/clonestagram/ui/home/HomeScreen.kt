5ba24f450ad586c8e177ace50a95b235825a001e
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-22 03:28:19 +0900

    - HomeScreen TopAppBar 추가

--- app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
@@ -1,11 +1,69 @@
 package com.gyleedev.clonestagram.ui.home
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.gyleedev.clonestagram.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     modifier: Modifier
 ) {
-    
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                        modifier = Modifier.sizeIn(minWidth = 100.dp)
+                    ) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.instagram_text_logo),
+                            contentDescription = null,
+                            modifier = Modifier.fillMaxHeight()
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Home,
+                            contentDescription = null,
+                        )
+                    }
+                    IconButton(
+                        onClick = { /*TODO*/ },
+                    ) {
+                        Icon(
+                            imageVector = Icons.Outlined.Home,
+                            contentDescription = null,
+                        )
+                    }
+                },
+                modifier = Modifier
+            )
+        },
+        modifier = modifier
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+        }
+    }
 }
