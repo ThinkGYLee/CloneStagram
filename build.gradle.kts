cd404da94a86234f485b4ba15430ff4f62bd148f
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-23 17:44:19 +0900

    - navigation, spotless dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,29 @@
+import com.diffplug.gradle.spotless.SpotlessExtension
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.hilt) apply false
     alias(libs.plugins.ksp) apply false
+    alias(libs.plugins.spotless) apply false
 }
 
+subprojects {
+    plugins.apply("com.diffplug.spotless")
+
+    configure<SpotlessExtension> {
+        kotlin {
+            target("**/*.kt")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+        kotlinGradle {
+            target("**/*.gradle.kts")
+            ktlint()
+            indentWithSpaces()
+            endWithNewline()
+        }
+    }
+}
