5bbea200c83d51efc2ebdd7850e77de0dccffece
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-23 22:26:02 +0900

    - homeScreen 스토리 칸 추가 - topAppbar nestedScroll 추가

--- app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
@@ -1,18 +1,32 @@
 package com.gyleedev.clonestagram.ui.home
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.sizeIn
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.filled.AddCircle
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import com.gyleedev.clonestagram.R
@@ -22,9 +36,13 @@ import com.gyleedev.clonestagram.R
 fun HomeScreen(
     modifier: Modifier
 ) {
+    val verticalScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
     Scaffold(
         topBar = {
             TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    scrolledContainerColor = TopAppBarDefaults.topAppBarColors().containerColor
+                ),
                 title = {
                     IconButton(
                         onClick = { /*TODO*/ },
@@ -42,28 +60,138 @@ fun HomeScreen(
                         onClick = { /*TODO*/ },
                     ) {
                         Icon(
-                            imageVector = Icons.Outlined.Home,
+                            painter = painterResource(id = R.drawable.heart_svgrepo_com),
                             contentDescription = null,
+                            modifier = Modifier.size(24.dp)
                         )
                     }
                     IconButton(
                         onClick = { /*TODO*/ },
                     ) {
                         Icon(
-                            imageVector = Icons.Outlined.Home,
+                            painter = painterResource(id = R.drawable.facebook_messenger_2880),
                             contentDescription = null,
+                            modifier = Modifier.size(24.dp)
                         )
                     }
                 },
-                modifier = Modifier
+                modifier = Modifier,
+                scrollBehavior = verticalScrollBehavior
             )
         },
-        modifier = modifier
+        modifier = modifier.nestedScroll(verticalScrollBehavior.nestedScrollConnection)
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState())
         ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(horizontal = 24.dp)
+                    .horizontalScroll(rememberScrollState())
+            ) {
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(end = 20.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icons8_test_account_96),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.Center)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null,
+                        tint = Color.Blue,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
+                }
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(end = 20.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icons8_test_account_96),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.Center)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null,
+                        tint = Color.Blue,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
+                }
 
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(end = 20.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icons8_test_account_96),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.Center)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null,
+                        tint = Color.Blue,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(end = 20.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icons8_test_account_96),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.Center)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null,
+                        tint = Color.Blue,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
+                }
+
+                Box(
+                    modifier = Modifier
+                        .wrapContentSize()
+                        .padding(end = 20.dp)
+                ) {
+                    Image(
+                        painter = painterResource(id = R.drawable.icons8_test_account_96),
+                        contentDescription = null,
+                        modifier = Modifier
+                            .size(80.dp)
+                            .align(Alignment.Center)
+                    )
+                    Icon(
+                        imageVector = Icons.Filled.AddCircle,
+                        contentDescription = null,
+                        tint = Color.Blue,
+                        modifier = Modifier.align(Alignment.BottomEnd)
+                    )
+                }
+            }
         }
     }
 }
