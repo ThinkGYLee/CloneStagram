686c151abd0ac42f5a8f0f88b1b8485b7f6e4807
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-27 03:15:18 +0900

    - myprofileScreen으로 바뀐거 반영

--- app/src/main/java/com/gyleedev/clonestagram/ui/CloneStagramScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/CloneStagramScreen.kt
@@ -40,8 +40,8 @@ import com.gyleedev.clonestagram.R
 import com.gyleedev.clonestagram.ui.detail.DetailScreen
 import com.gyleedev.clonestagram.ui.detail.ReelsScreen
 import com.gyleedev.clonestagram.ui.home.HomeScreen
-import com.gyleedev.clonestagram.ui.search.SearchScreen
 import com.gyleedev.clonestagram.ui.myProfile.MyProfileScreen
+import com.gyleedev.clonestagram.ui.search.SearchScreen
 import com.gyleedev.clonestagram.ui.upload.UploadScreen
 
 sealed class BottomNavItem(
