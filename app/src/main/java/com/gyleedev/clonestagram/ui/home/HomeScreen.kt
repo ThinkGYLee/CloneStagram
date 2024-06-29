154882dbb030cee46e5d7c5f111ed5f4efb7f916
ThinkGYLee <ntnpholicfp@gmail.com>
2024-06-30 02:20:10 +0900

    - 분리한 코드들 반영해서 homeScreen에 적용

--- app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/home/HomeScreen.kt
@@ -1,7 +1,5 @@
 package com.gyleedev.clonestagram.ui.home
 
-import android.annotation.SuppressLint
-import androidx.compose.animation.core.Animatable
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
@@ -9,8 +7,6 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.horizontalScroll
-import androidx.compose.foundation.interaction.MutableInteractionSource
-import androidx.compose.foundation.interaction.PressInteraction
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -39,7 +35,6 @@ import androidx.compose.foundation.text2.input.rememberTextFieldState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material.icons.outlined.MoreVert
 import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledIconButton
@@ -55,18 +50,14 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -76,11 +67,9 @@ import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.gyleedev.clonestagram.R
-import com.skydoves.landscapist.ImageOptions
-import com.skydoves.landscapist.coil.CoilImage
-import com.skydoves.landscapist.components.rememberImageComponent
-import com.skydoves.landscapist.placeholder.shimmer.Shimmer
-import com.skydoves.landscapist.placeholder.shimmer.ShimmerPlugin
+import com.gyleedev.clonestagram.ui.public.CommentInformation
+import com.gyleedev.clonestagram.ui.public.ItemData
+import com.gyleedev.clonestagram.ui.public.PublicItemDetail
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -89,12 +78,6 @@ fun HomeScreen(
     homeViewModel: HomeViewModel = hiltViewModel()
 ) {
     val verticalScrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
-    val isHeartTrue = remember {
-        mutableStateOf(false)
-    }
-    val isBookMarkTrue = remember {
-        mutableStateOf(false)
-    }
 
     val isCommentBottomSheetTrue = remember {
         mutableStateOf(false)
@@ -247,189 +230,13 @@ fun HomeScreen(
                 }
             }
             // 게시물
-            Column(
+
+            PublicItemDetail(
+                itemData = ItemData.initialItem,
+                comments = comments.value,
+                onBottomSheetStateChange = { isCommentBottomSheetTrue.value = true },
                 modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(vertical = 20.dp)
-            ) {
-                // 게시물 제목
-                Row(
-                    modifier = Modifier.fillMaxWidth(),
-                    horizontalArrangement = Arrangement.Center
-                ) {
-                    Box(
-                        modifier = Modifier
-                            .wrapContentSize()
-                            .padding(start = 16.dp, end = 20.dp)
-                            .border(
-                                width = 1.5.dp,
-                                brush = Brush.linearGradient(
-                                    colors = listOf(Color.Yellow, Color.Red),
-                                    start = Offset(0f, 0f),
-                                    end = Offset(70f, 70f)
-                                ),
-                                shape = CircleShape
-                            )
-                    ) {
-                        Box(
-                            modifier = Modifier
-                                .wrapContentSize()
-                                .padding(4.dp)
-                        ) {
-                            Image(
-                                painter = painterResource(id = R.drawable.icons8_test_account_96),
-                                contentDescription = null,
-                                modifier = Modifier.size(36.dp)
-                            )
-                        }
-                    }
-                    Column(modifier = Modifier.wrapContentSize()) {
-                        Text("think_gy_lee", fontWeight = FontWeight.SemiBold)
-                        Text(
-                            "♫ 뎁트・타이밍 (feat 김뮤지엄 & 에이민)",
-                            style = MaterialTheme.typography.labelMedium
-                        )
-                    }
-                    Spacer(modifier = Modifier.weight(5f))
-                    Icon(
-                        imageVector = Icons.Outlined.MoreVert,
-                        contentDescription = null,
-                        modifier = Modifier
-                            .align(Alignment.CenterVertically)
-                            .wrapContentSize()
-                            .padding(end = 8.dp)
-                            .clickable { }
-                    )
-                }
-
-                CoilImage(
-                    imageModel = { "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgX98TKIsaJF7D4wnq7YBOuMjtYH-6D5Kgm7m7VbRek7cQIGN7TNVtJMDIbSiEG5KgcGyGpgGxEOz7u9v-WhQASrQrjvCQF8-RQ7PsZpA6djqK7RA7mXrnt6aYiac8voLef_mhP-s_TucPVEP1vvmUBjspmjA2RdrbvIqVwYXQJZ1fwPyamJIxXTrgMVmg/s1600/image1.png" },
-                    imageOptions = ImageOptions(
-                        contentScale = ContentScale.Crop,
-                        alignment = Alignment.Center
-                    ),
-                    component = rememberImageComponent {
-                        +ShimmerPlugin(
-                            Shimmer.Flash(
-                                baseColor = Color.White,
-                                highlightColor = Color.LightGray
-                            )
-                        )
-                    },
-                    modifier = Modifier.fillMaxWidth()
-                )
-
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    if (isHeartTrue.value) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.heart_filled_svgrepo_com__1_),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(start = 20.dp)
-                                .size(24.dp)
-                                .bounceClick(
-                                    onClick = {},
-                                    onClickEnd = { isHeartTrue.value = !isHeartTrue.value }
-                                ),
-                            tint = Color.Red
-                        )
-                    } else {
-                        Icon(
-                            painter = painterResource(id = R.drawable.heart_svgrepo_com),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(start = 20.dp)
-                                .size(24.dp)
-                                .bounceClick(
-                                    onClick = {},
-                                    onClickEnd = { isHeartTrue.value = !isHeartTrue.value }
-                                ),
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-
-                    Icon(
-                        painter = painterResource(id = R.drawable.instagram_comment_13416),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(start = 20.dp)
-                            .size(24.dp)
-                    )
-                    Icon(
-                        painter = painterResource(id = R.drawable.instagram_share_13423),
-                        contentDescription = null,
-                        modifier = Modifier
-                            .padding(start = 20.dp)
-                            .size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.weight(10f))
-                    if (isBookMarkTrue.value) {
-                        Icon(
-                            painter = painterResource(id = R.drawable.black_save_instagram_18316),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(end = 12.dp)
-                                .size(24.dp)
-                                .bounceClick(
-                                    onClick = {},
-                                    onClickEnd = { isBookMarkTrue.value = !isBookMarkTrue.value }
-                                )
-                        )
-                    } else {
-                        Icon(
-                            painter = painterResource(id = R.drawable.save_instagram_black_lineal_18315),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .padding(end = 12.dp)
-                                .size(24.dp)
-                                .bounceClick(
-                                    onClick = {},
-                                    onClickEnd = { isBookMarkTrue.value = !isBookMarkTrue.value }
-                                )
-                        )
-                    }
-                }
-                Spacer(modifier = Modifier.height(12.dp))
-                Text(
-                    "좋아요 3658개",
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                )
-                Spacer(modifier = Modifier.height(2.dp))
-                Row(modifier = Modifier.fillMaxWidth()) {
-                    Text(
-                        "think_gy_lee",
-                        modifier = Modifier.padding(start = 20.dp),
-                        fontWeight = FontWeight.Bold
-                    )
-                    Spacer(modifier = Modifier.width(4.dp))
-                    Text("Android 15 is coming")
-                }
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    "댓글 ${comments.value.size}개 모두 보기",
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                        .clickable {
-                            isCommentBottomSheetTrue.value = true
-                        },
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
-                    fontWeight = FontWeight.Light
-                )
-                Spacer(modifier = Modifier.height(4.dp))
-                Text(
-                    "2시간 전",
-                    modifier = Modifier
-                        .padding(start = 20.dp)
-                        .clickable {
-                            isCommentBottomSheetTrue.value = true
-                        },
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = MaterialTheme.colorScheme.onSurface.copy(alpha = 0.8f),
-                    fontWeight = FontWeight.Light
-                )
-            }
+            )
         }
 
         if (isCommentBottomSheetTrue.value) {
@@ -443,50 +250,6 @@ fun HomeScreen(
             )
         }
     }
-}
-
-// bounceClick
-// TODO supperessLint 수정할것
-@SuppressLint("ModifierFactoryUnreferencedReceiver")
-private fun Modifier.bounceClick(
-    scaleDown: Float = 0.30f,
-    onClick: () -> Unit,
-    onClickEnd: () -> Unit
-) = composed {
-    val interactionSource = remember { MutableInteractionSource() }
-
-    val animatable = remember {
-        Animatable(1f)
-    }
-
-    LaunchedEffect(interactionSource) {
-        interactionSource.interactions.collect { interaction ->
-            when (interaction) {
-                is PressInteraction.Press -> animatable.animateTo(scaleDown)
-                is PressInteraction.Release -> animatable.animateTo(scaleDown)
-                is PressInteraction.Cancel -> animatable.animateTo(1f)
-            }
-        }
-    }
-
-    LaunchedEffect(animatable.value) {
-        if (animatable.value == scaleDown) {
-            onClickEnd()
-        }
-    }
-
-    Modifier
-        .graphicsLayer {
-            val scale = animatable.value
-            scaleX = scale
-            scaleY = scale
-        }
-        .clickable(
-            interactionSource = interactionSource,
-            indication = null
-        ) {
-            onClick()
-        }
 }
 
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalFoundationApi::class)
@@ -675,15 +438,6 @@ private fun CommentModalBottomSheet(
         }
     }
 }
-
-data class CommentInformation(
-    val userImage: Int,
-    val userId: String,
-    val writtenHourAgo: Int,
-    val isWrittenByAuthor: Boolean,
-    val content: String,
-    val heartCount: Int
-)
 
 @Composable
 private fun CommentItem(
