f2a8c4eacfda42eb539e1f5882a083a6dd3b81c8
ThinkGYLee <ntnpholicfp@gmail.com>
2024-07-01 23:05:13 +0900

    - searchSceen scaffold 세팅

--- app/src/main/java/com/gyleedev/clonestagram/ui/search/SearchSceen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/search/SearchSceen.kt
@@ -1,5 +1,9 @@
 package com.gyleedev.clonestagram.ui.search
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
@@ -7,4 +11,15 @@ import androidx.compose.ui.Modifier
 fun SearchScreen(
     modifier: Modifier
 ) {
+    Scaffold(
+        topBar = {},
+        modifier = modifier
+    ) { innerPadding ->
+        Column(modifier = Modifier
+            .padding(innerPadding)
+            .consumeWindowInsets(innerPadding)) {
+
+        }
+
+    }
 }
