dd3733fbb723b6dfe3d96663a1a8cbb006f2bdc6
ThinkGYLee <ntnpholicfp@gmail.com>
2024-07-10 01:34:03 +0900

    - updateScreen 의미없는 print 제거

--- app/src/main/java/com/gyleedev/clonestagram/ui/upload/UploadScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/upload/UploadScreen.kt
@@ -1,68 +1,12 @@
 package com.gyleedev.clonestagram.ui.upload
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun UploadScreen(
-    modifier: Modifier
+    modifier: Modifier = Modifier
 ) {
-    Column {
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-        Text(text = "black")
-    }
+
+
 }
