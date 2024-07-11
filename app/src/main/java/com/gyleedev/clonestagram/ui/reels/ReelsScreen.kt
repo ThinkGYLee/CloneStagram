53751d48611886f6c04ced14a2f30ce4dc47490d
ThinkGYLee <ntnpholicfp@gmail.com>
2024-07-11 23:11:41 +0900

    - ReelsScreen Icon 공통 컴포넌트로 처리

--- app/src/main/java/com/gyleedev/clonestagram/ui/reels/ReelsScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/reels/ReelsScreen.kt
@@ -1,7 +1,6 @@
 package com.gyleedev.clonestagram.ui.reels
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
@@ -16,7 +15,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowDown
@@ -28,14 +26,16 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.gyleedev.clonestagram.R
+import com.gyleedev.clonestagram.ui.public.UserIconDefinition
+import com.gyleedev.clonestagram.ui.public.UserIconImageType
+import com.gyleedev.clonestagram.ui.public.UserIconType
+import com.gyleedev.clonestagram.ui.public.UserImageComponent
 import com.skydoves.landscapist.ImageOptions
 import com.skydoves.landscapist.coil.CoilImage
 import com.skydoves.landscapist.components.rememberImageComponent
@@ -78,25 +78,14 @@ fun ReelsScreen(
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    Box(
-                        modifier = Modifier.border(
-                            width = 2.dp,
-                            brush = Brush.linearGradient(
-                                colors = listOf(Color.Yellow, Color.Red),
-                                start = Offset(0f, 0f),
-                                end = Offset(70f, 70f)
-                            ),
-                            shape = CircleShape
+                    UserImageComponent(
+                        userIconDefinition = UserIconDefinition(
+                            iconImageType = UserIconImageType.IconFromDrawableType(R.drawable.icons8_test_account_48),
+                            hasStory = true,
+                            userIconType = UserIconType.IconReels()
                         )
-                    ) {
-                        Box(modifier = Modifier.padding(4.dp)) {
-                            Image(
-                                painter = painterResource(id = R.drawable.icons8_test_account_48),
-                                contentDescription = null,
-                                modifier = Modifier.size(32.dp)
-                            )
-                        }
-                    }
+                    )
+
                     Spacer(modifier = Modifier.width(8.dp))
                     Text(text = "think_gy_lee", color = Color.White, fontWeight = FontWeight.Bold)
                     Spacer(modifier = Modifier.width(8.dp))
