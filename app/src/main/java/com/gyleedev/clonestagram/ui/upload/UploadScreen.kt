f23be9d98ba1b33dfba3a89a8821df0e6dc53b45
ThinkGYLee <ntnpholicfp@gmail.com>
2024-07-16 23:19:34 +0900

    - UploadScreen 탑바

--- app/src/main/java/com/gyleedev/clonestagram/ui/upload/UploadScreen.kt
+++ app/src/main/java/com/gyleedev/clonestagram/ui/upload/UploadScreen.kt
@@ -1,22 +1,53 @@
 package com.gyleedev.clonestagram.ui.upload
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun UploadScreen(
     modifier: Modifier = Modifier
 ) {
-    Scaffold(modifier = modifier) { innerPadding ->
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                navigationIcon = {
+                    IconButton(onClick = { /*TODO*/ }) {
+                        Icon(imageVector = Icons.Filled.Close, contentDescription = null)
+                    }
+                },
+                title = { Text(text = "새 게시글") },
+                actions = {
+                    Text(
+                        text = "다음",
+                        color = Color.Blue,
+                        modifier = Modifier
+                            .padding(end = 12.dp)
+                            .clickable { })
+                })
+
+        },
+        modifier = modifier
+    ) { innerPadding ->
         Column(
             Modifier
                 .padding(innerPadding)
-                .consumeWindowInsets(innerPadding)) {
-
+                .consumeWindowInsets(innerPadding)
+        ) {
         }
     }
 }
